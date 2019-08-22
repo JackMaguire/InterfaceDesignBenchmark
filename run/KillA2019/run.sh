590cb290574a5794c68e9939e15271b226ee2af1
Jack <jackmaguire1444@gmail.com>
2019-08-22 16:32:44 -0400

    KillA2019

---
+++ run/KillA2019/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ 
+#SBATCH --time=3-0:00:00
+#SBATCH -p skylake,528_queue
+#SBATCH -N 2
+#SBATCH --ntasks-per-node=40
+#SBATCH -o XXX.%j.log
+
+input_struct=XYZ
+srun --mpi=pmi2 -n 80 rosetta_scripts.mpiserialization.linuxgccrelease -s $input_struct -mute all -mpi_tracer_to_file mpi -ignore_unrecognized_res 1 -nstruct 1000 -parser:protocol ../../scripts/run.xml -parser:script_vars script=KillA2019
+ 
+# END FILE HERE
--- run/legacy/run.sh
+++ run/legacy/run.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks-per-node=40
 #SBATCH -o XXX.%j.log
 
-input_struct=$1
+input_struct=XYZ
 srun --mpi=pmi2 -n 80 rosetta_scripts.mpiserialization.linuxgccrelease -s $input_struct -mute all -mpi_tracer_to_file mpi -ignore_unrecognized_res 1 -nstruct 1000 -parser:protocol ../../scripts/run.xml -parser:script_vars script=legacy
  
 # END FILE HERE
