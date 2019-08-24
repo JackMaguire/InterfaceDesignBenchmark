c67077bfb36b902aca66801edee8979f456ef556
Jack <jackmaguire1444@gmail.com>
2019-08-24 08:23:23 -0400

    fixed submission scripts

--- run/hbnet_KillA2019/run.sh
+++ run/hbnet_KillA2019/run.sh
@@ -6,7 +6,10 @@
 #SBATCH --ntasks-per-node=40
 #SBATCH -o XXX.%j.log
 
+dir=ABC
 input=XYZ
-srun --mpi=pmi2 -n 80 rosetta_scripts.mpiserialization.linuxgccrelease -l $input -mute all -mpi_tracer_to_file mpi -ignore_unrecognized_res 1 -nstruct 10 -parser:protocol ../../scripts/run.xml -parser:script_vars script=KillA2019
+
+cd $dir
+srun --mpi=pmi2 -n 80 rosetta_scripts.mpiserialization.linuxgccrelease -l $input -mute all -mpi_tracer_to_file mpi -ignore_unrecognized_res 1 -nstruct 10 -parser:protocol /nas/longleaf/home/jackmag/scratch/InterfaceDesignBenchmark/scripts/run.xml -parser:script_vars script=KillA2019 -out:prefix hbnet_KillA2019_
  
 # END FILE HERE
--- run/hbnet_KillA2019/submit.sh
+++ run/hbnet_KillA2019/submit.sh
@@ -2,9 +2,13 @@
 
 for x in a b c d e f; do
     for y in 1 2 3; do
+	while [[ `squeue -u jackmag | grep jackmag | wc -l` -gt 39 ]]; do
+	    sleep 600
+	done
 	name=$x$y
-	pdblist=hbnet_structures/a1/a1.top_100_hbnet_pdblist
-	cat run.sh | sed "s&XYZ&$pdblist&g" | sed "s\XXX\killa${name}\g" > run.$name.sh
+	dir=hbnet_structures/$name
+	pdblist=$name.top_100_hbnet_pdblist
+	cat run.sh | sed "s&XYZ&$pdblist&g" | sed "s\XXX\kh_${name}\g" | sed "s&ABC&$dir&g" > run.$name.sh
 	sbatch run.$name.sh
     done
 done
